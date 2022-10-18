a2973c3bec3b62a09896b713d92ab3c1001787fe
Noblelexy <adiroidowu1998@gmail.com>
2022-10-18 13:06:22 -0700

    my commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints the sum of even-valued Fibonacci sequence
- *        terms not exceeding 4000000.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
