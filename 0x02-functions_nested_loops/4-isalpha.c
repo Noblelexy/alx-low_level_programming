14872db0d457265bfdc7562477ee408c4879b415
Noblelexy <adiroidowu1998@gmail.com>
2022-10-18 20:19:05 -0700

    my commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 (SUCCESS)
- * Description: Write a function that checks for alphabetic character
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  *
  */
 int _isalpha(int c)
