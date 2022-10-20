89ba705ec437b4f0683faced8abb5d418dab7684
Noblelexy <adiroidowu1998@gmail.com>
2022-10-20 06:24:03 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print  0 - 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}
