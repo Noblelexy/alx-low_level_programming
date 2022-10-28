3e05c54fe0f9dca9e22465ec14fbc6f3028c404f
Noblelexy <adiroidowu1998@gmail.com>
2022-10-27 19:11:26 -0700

    my commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
@@ -18,7 +18,7 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 		/* ...so that this prints 98\n */
-		printf("a[2] = %d\n", a[2]);
+		_putchar("a[2] = %d\n", a[2]);
 		return (0);
 }
 
