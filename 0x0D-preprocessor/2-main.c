9b8d2200a11bbf1db133152c5365ff535287a622
Noblelexy <adiroidowu1998@gmail.com>
2022-11-14 18:55:33 -0800

    my commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
